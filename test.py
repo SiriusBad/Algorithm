30f05d7c3ea2a185fdd0f31e96174f5865249583
SiriusBad <wyzsirius@icloud.com>
2023-01-02 19:27:39 +0800

    sirius

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
-# import random
-# A = []
-# for i in range(1000):
-#     A.append(random.randint(1,1000))
-# print(A)
+import random
+n = 512
+A = [list() for i in range(n)]
+for i in range(n):
+    for j in range(n):
+        A[i].append(random.randint(10, 100))
+print(A)
 
+
+# import math
+# a = -math.inf
+# print(a<-10000000)
+
+# import numpy as np
+# mul = np.zeros((4,9))
+# # print(mul)
+# print(3*4)
